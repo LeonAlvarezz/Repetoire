dce7f6b1ad57f2ce9987b721e1a2ada2fa5d40c7
LeonAlvarezz <107019128+LeonAlvarezz@users.noreply.github.com>
2022-09-10 23:31:23 +0700

    Delete Repertoire directory

--- Repertoire/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.repertoire {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.repertoire to javafx.fxml;
-    exports com.example.repertoire;
-}
